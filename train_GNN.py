6eaedbecc25c2abb63d23618ee6add69c85a2bb1
root <gm_xiong@qq.com>
2020-04-04 15:12:11 +0800

    test

--- train_GNN.py
+++ train_GNN.py
@@ -107,7 +107,7 @@ def main(args):
     if args.cuda:
         if args.fp16: classifier, optimizer = amp.initialize(classifier, optimizer, opt_level=opt_level)
         classifier = BalancedDataParallel(args.gpu0_bsz // args.acc_grad, classifier, dim=0)
-        classifier = classifier.to(arg.device)
+        classifier = classifier.to(args.device)
         # torch.distributed.init_process_group(backend="nccl")
         # classifier = nn.parallel.DistributedDataParallel(classifier)
 
